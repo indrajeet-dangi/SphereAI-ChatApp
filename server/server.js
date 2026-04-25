78ccbaf64deddd951a64f75e6d4857611e10ad2c
indrajeet-dangi <indrajeet.dangi04@gmail.com>
2026-04-25 16:21:39 +0530

    added vercel.json in client

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ const allowedOrigins = (process.env.CLIENT_URL || "http://localhost:5173")
 
 app.use(
   cors({
-    origin: allowedOrigins ,
+    origin: allowedOrigins,
     credentials: true,
   })
 );
